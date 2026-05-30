4e1c61fe10c9b5920df1329434f160cde42c2353
Devanshi Bhatt <dbhatt2310@gmail.com>
2026-05-30 20:20:35 +0530

    [feat-Homepage]: Improve homepage UI

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.jsx'
 import { NotificationProvider } from './context/NotificationContext.jsx'
+import { ThemeProvider } from './context/ThemeContext.jsx'
 import { ToastContainer } from './components/Toast.jsx'
 import './index.css'
 import App from './App.jsx'
@@ -11,10 +12,12 @@ createRoot(document.getElementById('root')).render(
   // <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <NotificationProvider>
-          <App />
-          <ToastContainer />
-        </NotificationProvider>
+        <ThemeProvider>
+          <NotificationProvider>
+            <App />
+            <ToastContainer />
+          </NotificationProvider>
+        </ThemeProvider>
       </AuthProvider>
     </BrowserRouter>
   // {/* </StrictMode>, */}
